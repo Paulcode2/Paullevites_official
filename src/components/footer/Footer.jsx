6753b656811615be7448bde3c331a5814c7fcb3f
PaulCode2 <Paullevites84@gmail.com>
2024-01-09 15:49:05 +0100

    fixes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
            <div className="footer__socials">
             <a href="https://facebook.com/paul.Levite.73/"><AiFillFacebook/></a>
             <a href="https://X.com/curlyhair_dev"><AiFillTwitterCircle/></a>
-            <a href="https://instagram.com/Paullevites_official/"><AiFillInstagram/></a>
+            <a href="https://instagram.com/curlyhair_dev"><AiFillInstagram/></a>
             <a href="https://www.linkedin.com/in/paul-ifeoluwa-levites-4a1530262/"><AiFillLinkedin/></a>
             <a href="https://github.com/Paulcode2"><AiFillGithub/></a>
             </div> 
--- src/components/header/Headersocials.jsx
+++ src/components/header/Headersocials.jsx
@@ -8,7 +8,7 @@ const Headersocials = () => {
         <div className="Header__Socials">
             <a href="https://facebook.com/paul.Levite.73/" target="_blank" rel="noreferrer"><AiFillFacebook /></a>
             <a href="https://X.com/curlyhair_dev" target="_blank" rel="noreferrer"><AiFillTwitterCircle /></a>
-            <a href="https://instagram.com/Paullevites_official/" target="_blank" rel="noreferrer"><AiFillInstagram /></a>
+            <a href="https://instagram.com/curlyhair_dev" target="_blank" rel="noreferrer"><AiFillInstagram /></a>
         </div>
     )
 }
